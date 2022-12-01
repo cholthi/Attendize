44d31f2de60155070623c6981f809d1acd05bf91
cholthi <cholthipaul@gmail.com>
2022-12-01 08:56:41 +0200

    offline tickets are marked as paid from creation. this is to distinguish them from online tickets that failed payment

--- resources/views/Public/HomePage/Partials/BannerSlider.blade.php
+++ resources/views/Public/HomePage/Partials/BannerSlider.blade.php
@@ -6,7 +6,7 @@
       @foreach($events->get() as $event)
         <div>
             <a href="{{$event->event_url}}" target="_self">
-               @if(file_exists($event->images-first()->banner_image_path()))
+               @if(file_exists($event->images->first()->banner_image_path()))
                 <img src="{{ asset($event->images->first()->banner_image_path()) }}"
                     alt="{{ $event->title}}" />
                @endif
