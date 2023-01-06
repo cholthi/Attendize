cbc69d7bb265275f1d1251ee9c6d834aa582ae42
Chol Paul <cholthipaul@gmail.com>
2023-01-06 10:43:06 +0200

    added local whatsapp number and update static page

--- resources/views/Public/StaticPage/HowToBuy.blade.php
+++ resources/views/Public/StaticPage/HowToBuy.blade.php
@@ -4,14 +4,51 @@
 @stop
 
 @section('content')
-<section class="mx-auto bg-white container" style="color:black; width:80%;">
-<h5 class="text-center mb-4 pb-2 title">How to buy Tickets on Ticketana</h5>
+<section class="bg-white container mx-auto" style="color:black;width:80%;">
+<div class="card shadow">
+          <div class="card-header bg-primary text-white">
+            <h2 class="h3 mb-3 px-2 text-center">How to buy ticket online</h2>
+          </div>
+          <div class="card-body px-2">
+            <ol class="instructions list-unstyled">
+              <li class="mb-3">
+                <h6>
+                  <span>Step 1.</span> <a href="{{route('home')}}" class="text-primary">Find</a> your event in the upcoming event list and click on it.
+                </h6>
+                <p>
+                  <img class="mt-5 mb-5 img-fluid" src="{{asset('assets/images/static/0.png')}}" alt="Find Ticket" title="Find Ticket">
+                </p>
+              </li>
+              <li class="mb-3">
+                <h6>
+                    Step 2. On the event page, choose the ticket type and quantity of tickets you want to buy and click "Buy Ticket" button.
+                </h6>
+                <p>
+                  <img class="mt-5 mb-5 img-fluid" src="{{asset('assets/images/static/1.png')}}" alt="Tickets" title="Tickets">
+                </p>
+              </li>
+              <li class="mb-3">
+                <h6>Step 3. Enter your information to appear in the ticket and click "Continue to payment".</h6>
+                <p>
+                  <img class="mt-5 mb-5 img-fluid" src="{{asset('assets/images/static/2.png')}}" alt="Payment" title="Payment">
+                </p>
+              </li>
+              <li class="mb-3">
+                <h6>Step 4. Choose your payment method of your choice among the provided one and click "Complete Payment".</h6>
+                <p>
+                  <img class="mt-5 mb-5 img-fluid" src="{{asset('assets/images/static/3.png')}}" alt="Buy">
+                </p>
+              </li>
+              <li class="mb">
+                <h6>Step 5. Upon successful payment, you will receive an email from Ticketana with ticket attached to it. You can print it or have the pdf in your phone to show the event check in.</h6>
+                <p>
+                  <img class="mt-5 mb-5 img-fluid" src="{{asset('assets/images/static/4.png')}}" alt="Email">
+                </p>
+              </li>
+            </ol>
+          </div>
+        </div>
 
-  <div class="row">
-
-
-</div>
-</div>
 </section>
 @stop
 
--- resources/views/Shared/Partials/WatsappButton.blade.php
+++ resources/views/Shared/Partials/WatsappButton.blade.php
@@ -9,7 +9,7 @@
 <script type="text/javascript">
    $(function () {
            $('#WAButton').floatingWhatsApp({
-               phone: '254718458901', //WhatsApp Business phone number
+               phone: '211917821026', //WhatsApp Business phone number
                headerTitle: 'Chat with us on WhatsApp!', //Popup Title
                popupMessage: 'Hello, how can we help you?', //Popup Message
                showPopup: true, //Enables popup display
